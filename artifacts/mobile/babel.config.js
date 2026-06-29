b1fc3fb4b717a0a30dbb25fe5889300f043e3e2b
Uselessbott <charansadimela0@gmail.com>
2026-06-29 05:55:24 +0530

    fix: add babel plugins for private class properties

--- artifacts/mobile/babel.config.js
+++ artifacts/mobile/babel.config.js
@@ -2,5 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
+    plugins: [
+      "@babel/plugin-proposal-private-methods",
+      "@babel/plugin-proposal-class-properties",
+    ],
   };
 };
