f5858939ec881d762bda2a61f740f401ecfd3256
Uselessbott <charansadimela0@gmail.com>
2026-06-29 23:11:28 +0530

    Fix Babel plugin conflict

--- artifacts/mobile/babel.config.js
+++ artifacts/mobile/babel.config.js
@@ -1,10 +1,8 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
-    plugins: [
-      "@babel/plugin-proposal-private-methods",
-      "@babel/plugin-proposal-class-properties",
-    ],
+    presets: [
+      ["babel-preset-expo", { unstable_transformImportMeta: true }]
+    ]
   };
 };
